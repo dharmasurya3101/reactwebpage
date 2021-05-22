cac307f67c97b4e5e56821a8b27ac296d21c6983
dharmasurya3101 <58599226+dharmasurya3101@users.noreply.github.com>
2021-05-22 20:05:30 +0530

    Add files via upload

---
+++ suryaSwagruhaPickles.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './styleSuryaPickles.css';
+
+function SuryaSwagruhaPickles(){
+    return(
+           <React.Fragment>
+             <header className="bgcolor">
+	            <div id="header">
+	                <a className="header1" href="tel:000000000"> Phone: 0000000000 </a>
+	                <a className="header1" href="mailto:dharmasurya3101@gmail.com"> Email: dharmasurya3101@gmail.com </a>
+	                <a className="header1" > Working Hours: 9:00 - 21:00 </a>
+            	</div>
+                
+                <div id="navbar">
+                   <ul class="navig">
+                    <h5>Surya Swagruha Pickles</h5>
+                    
+	                <li className="navi"> <a href="index.html"> HOME </a> </li> 
+                	<li className="navi"> <a href="aboutus.html">ABOUT US </a></li> 
+                	<li className="navi">OUR SERVICES </li>
+	                <li className="navi"><a href="ourproject.html">PRODUCTS </a></li>
+	                <li className="navi">OUR PACKAGES</li>
+                	<li className="navi"><a href="contactus.html">CONTACT US </a></li>
+                    
+            	 </ul>
+                 
+	            </div>
+             </header>
+            </React.Fragment> 
+         
+
+    );
+}
+  export default SuryaSwagruhaPickles ;
---
+++ suryaSwagruhaPicklesBody.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './styleSuryaPickles.css';
+
+function SuryaSwagruhaPicklesBody(){
+    return(
+           <React.Fragment>
+               <div id="sec" Style="background-image: url('https://akm-img-a-in.tosshub.com/indiatoday/images/story/201512/story_pickle_647_122915122250.jpg?size=770:433')">
+		
+                 <div id="sec2">  
+                     
+                      Our Pickles are <br />
+                     &nbsp; &nbsp; &nbsp; &nbsp;<span > Home -Made </span>
+                     Products, <br /> 
+                     <h2 Style="text-align:right; padding-left:500px"> Manufactured In Hyderabad.</h2>
+                </div>
+               </div> 
+           </React.Fragment>
+    );
+}
+
+export default SuryaSwagruhaPicklesBody;
+
+
